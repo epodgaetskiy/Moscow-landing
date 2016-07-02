1965bfa31a4cbd222622de259d5c332a57dc4ae7
Evgeniy Podgaetskiy <epodgaetskiy@gmail.com>
2016-07-02 22:31:19 +0300

    productiin

---
+++ js/common.js
@@ -0,0 +1,26 @@
+$(function() {
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+
+	$(".btn-click").click(function() {
+		$(".burger-menu").toggle();
+	})
+});
